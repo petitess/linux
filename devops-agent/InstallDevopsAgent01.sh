9f91abe4609809679e9be1f66c29bcf7428817c9
Karol <93782725+petitess@users.noreply.github.com>
2024-03-27 12:35:34 +0100

    Update InstallDevopsAgent01.sh

--- devops-agent/InstallDevopsAgent01.sh
+++ devops-agent/InstallDevopsAgent01.sh
@@ -1,25 +1,25 @@
 installAgent() {
-    local PAT="xceks4xf5seh5vutzt27j2hsosvq"
-    local ORG="xxxse"
+    local PAT="xvceks4xf5seh5vutzt27j2hsosvq"
+    local ORG="xxse"
     local POOL="vmdevopslabb01"
-    sudo mkdir devopsagentss
+    sudo mkdir devopsagents
     sudo groupadd agentusers
     sudo gpasswd -a azadmin agentusers
     sudo gpasswd -a root agentusers
-    cd devopsagentss
-    sudo curl --output agent.tar.gz "https://vstsagentpackage.azureedge.net/agent/3.234.0/vsts-agent-linux-x64-3.234.0.tar.gz"
-    for i in {2..5}
+    cd devopsagents
+    sudo curl --output agent.tar.gz "https://vstsagentpackage.azureedge.net/agent/3.236.1/vsts-agent-linux-x64-3.236.1.tar.gz"
+    for i in {1..2}
     do
     sudo mkdir "agent$i"
     cd "agent$i"
     sudo tar zxvf ../agent.tar.gz
-    sudo chown -R azadmin:agentusers /devopsagentss
-    sudo chown -R azadmin:agentusers /devopsagentss
-    sudo chmod -R g+w /devopsagentss
-    ./config.sh --unattended --url https://dev.azure.com/$ORG --auth pat --token $PAT --pool $POOL --agent "agent$i" --acceptTeeEula --work "../../tmp/_work_agent$i"
+    sudo chown -R azadmin:agentusers /devopsagents
+    sudo chown -R azadmin:agentusers /devopsagents
+    sudo chmod -R g+w /devopsagents
+    ./config.sh --unattended --url https://dev.azure.com/$ORG --auth pat --token $PAT --pool $POOL --agent "agent_$(date '+%Y_%m_%d')_$i" --acceptTeeEula --work "../../tmp/_work_agent$i"
     sudo ./svc.sh install
     sudo ./svc.sh start
     cd ..
     done
 }
-installAgent 
+installAgent
