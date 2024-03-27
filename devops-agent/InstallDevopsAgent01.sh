26b76c662de71444831f188d9c8478a4c5f08dbc
Karol <93782725+petitess@users.noreply.github.com>
2024-03-27 10:51:48 +0100

    install multiple agents

--- devops-agent/InstallDevopsAgent01.sh
+++ devops-agent/InstallDevopsAgent01.sh
@@ -1,20 +1,21 @@
 installAgent() {
     local PAT="xceks4xf5seh5vutzt27j2hsosvq"
-    local ORG="xse"
+    local ORG="xxxse"
     local POOL="vmdevopslabb01"
-    sudo mkdir devopsagents
+    sudo mkdir devopsagentss
     sudo groupadd agentusers
     sudo gpasswd -a azadmin agentusers
     sudo gpasswd -a root agentusers
-    cd devopsagents
+    cd devopsagentss
     sudo curl --output agent.tar.gz "https://vstsagentpackage.azureedge.net/agent/3.234.0/vsts-agent-linux-x64-3.234.0.tar.gz"
-    for i in {1..2}
+    for i in {2..5}
     do
     sudo mkdir "agent$i"
     cd "agent$i"
     sudo tar zxvf ../agent.tar.gz
-    sudo chown -R azadmin:agentusers /devopsagents
-    sudo chmod -R g+w /devopsagents
+    sudo chown -R azadmin:agentusers /devopsagentss
+    sudo chown -R azadmin:agentusers /devopsagentss
+    sudo chmod -R g+w /devopsagentss
     ./config.sh --unattended --url https://dev.azure.com/$ORG --auth pat --token $PAT --pool $POOL --agent "agent$i" --acceptTeeEula --work "../../tmp/_work_agent$i"
     sudo ./svc.sh install
     sudo ./svc.sh start
